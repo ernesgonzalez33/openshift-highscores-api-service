33dd6f4c3378bd354ea212365652b93e6c66cb73
dudash <jason.dudash@gmail.com>
2020-10-13 15:52:38 -0400

    added checksums to the highscores to validate client data hasn't been tampered with

--- src/main/java/io/nub3s/Score.java
+++ src/main/java/io/nub3s/Score.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Score extends PanacheMongoEntity {
     public String name;
     public Integer score;
+    public String checksum;
 
     public String getName(){
         return name;
